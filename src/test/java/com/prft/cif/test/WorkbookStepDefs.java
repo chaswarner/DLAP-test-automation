8a3272062e844d21aa445a9a028c8ee07b9d23e5
Warner <cwarne01@bcbsmamd.net>
2018-11-14 15:03:12 -0500

    Avinash column check code

--- src/test/java/com/prft/cif/test/WorkbookStepDefs.java
+++ src/test/java/com/prft/cif/test/WorkbookStepDefs.java
@@ -15,7 +15,15 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.codehaus.jettison.json.JSONArray;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import org.junit.Assert;
 import cucumber.api.java.After;
@@ -25,6 +33,7 @@ import java.io.File;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.ResourceBundle;
@@ -53,8 +62,11 @@ public class WorkbookStepDefs {
     private static Logger logger;
 
 
-    String wbFilePath = "./src/test/resources/fixtures/ME_FIN_Cash_Detail.xlsx";
+    String wbFilePath = "./src/test/resources/fixtures/Test_ME_FIN_Cash_Detail_DateFormatChange.xlsx";
     File wbfile = new File(wbFilePath);
+    ArrayList<String> columnname = new ArrayList<>();
+    ArrayList<String> columntype = new ArrayList<>();
+
 
     private static ResourceBundle rb = ResourceBundle.getBundle("cif");
 
@@ -149,6 +161,115 @@ public class WorkbookStepDefs {
 
     // Delete folder at /env/zone/domain/dataset
 
+    }
+    @Given("^I have parsed a curate workbook$")
+    public void parsing_data() throws Throwable {
+        MetadataWorkbook metadataWorkbook = CIFInjector.createInstance(MetadataWorkbook.class, "workbookmapping.properties");
+        dataset = metadataWorkbook.getDataset(wbfile, 0, "curate");
+        Thread.sleep(4000);
+        System.out.println("DATASET ::: " + dataset.toString());
+        System.out.println("DATASET ATTRIBUTES ::: " + dataset.getAttributes());
+        System.out.println("Table Name ::: " + dataset.getName());
+    }
+    @When("^I query Cloudera Navigator for the list of columns in the data set database$")
+
+    public void database_columns() throws Throwable {
+        Connection conn;
+        String DB_URL = "jdbc:hive2://hive.dr.bcbsma.com:10000/;principal=hive/hive.dr.bcbsma.com@BCBSMAMD.NET;ssl=true";
+        conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName("org.apache.hive.jdbc.HiveDriver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, "", "");
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "describe"+dataset.getName();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                //Retrieve by column name
+                String cname = rs.getString("Column Name");
+                String ctype = rs.getString("Column Type");
+
+                //Display values
+                System.out.print(", Column Name: " + cname);
+                columnname.add(cname);
+                System.out.println(", Column Type: " + ctype);
+                columntype.add(ctype);
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+//        String query="select rcpnt_id from"+dataset.getName();
+//        ArrayList<String> list = cifimpala.describeTable(query);
+////        int listSize=list.size();
+//        int mapSize=list.get(1);
+//
+//
+//        for (int listEntry = 0; listEntry < listSize; listEntry++) {
+//
+//            String[] values = new String[mapSize];
+//            Set entries = list.get(listEntry).entrySet();
+//            Iterator entriesIterator = entries.iterator();
+//            int i = 0;
+//            while (entriesIterator.hasNext()) {
+//
+//                Map.Entry mapping = (Map.Entry) entriesIterator.next();
+//                values[i] = mapping.getValue().toString();
+//                System.out.println(values[i]);
+//                ++i;
+//            }
+//
+
+    @Then("^I should see appropriate columns in the appropriate DB available via Impala$")
+    public void workbook_columns() throws Throwable {
+        String tableName = null;
+        ArrayList<String> curateColNames = new ArrayList<String>();
+        Workbook workbook = null;
+        try {
+            workbook = WorkbookFactory.create(wbfile);
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+        Sheet sheet = workbook.getSheetAt(0);
+        DataFormatter dataFormatter = new DataFormatter();
+        Row rownum = sheet.getRow(2);
+        Cell cellnum = rownum.getCell(1);
+        String cellval = dataFormatter.formatCellValue(cellnum);
+        System.out.println("Cell Value :: " + cellval);
+        ArrayList<String> CurateColumn = new ArrayList<String>();
+        ArrayList<String> CurateDataType = new ArrayList<String>();
+        for(int i=46; i<=sheet.getLastRowNum();i++) {
+            rownum = sheet.getRow(i);
+            cellnum = rownum.getCell(11);
+            String CurateCellValue = dataFormatter.formatCellValue(cellnum);
+            CurateColumn.add(CurateCellValue);
+            cellnum = rownum.getCell(12);
+            String CurateCellType = dataFormatter.formatCellValue(cellnum);
+            CurateDataType.add(CurateCellType);
+            System.out.println(CurateCellValue );
+        }
+        if(columnname.size()== CurateColumn.size() ) {
+            for (int i = 0; i < columnname.size(); i++) {
+                String actual = columnname.get(i);
+                String expected = CurateColumn.get(i);
+                assertEquals(actual, expected);
+            }
+        }
+//            if(columntype.size()== CurateDataType.size() ) {
+//                for (int i = 0; i < columntype.size(); i++) {
+//                    String actual = columntype.get(i);
+//                    String expected = CurateDataType.get(i);
+//                    assertEquals(actual, expected);
+//                }
+//            }
     }
 
 
