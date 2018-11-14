758d822f9e5ac57fec7660f80a715da83f6930d2
Warner <cwarne01@bcbsmamd.net>
2018-11-14 12:31:03 -0500

    pranav updates

--- src/test/java/com/prft/cif/test/WorkbookStepDefs.java
+++ src/test/java/com/prft/cif/test/WorkbookStepDefs.java
@@ -1,5 +1,6 @@
 package com.prft.cif.test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.prft.cif.test.conversion.MetadataWorkbook;
@@ -12,36 +13,81 @@ import com.prft.cif.test.util.CIFHDFSUtil;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.ContentSummary;
-import org.apache.hadoop.fs.FileSystem;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONObject;
+
 import org.junit.Assert;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
 import java.io.File;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class WorkbookStepDefs {
 
-//    CIFRestClient restClient = CIFInjector.createInstance(NavigatorRestClient.class);
+    //    CIFRestClient restClient = CIFInjector.createInstance(NavigatorRestClient.class);
     CIFRestClient restClient;
     String response;
     CIFDataset dataset;
-    CIFHDFSUtil hdfsUtil;
-    String wbFilePath = "./src/test/resources/fixtures/Test_ME_FIN_Cash_Detail_DateFormatChange.xlsx";
 
+/*    @Inject
+    private CIFHDFSUtil hdfsUtil;*/
+
+
+    private String onboardingDir;
+    //    private String onboardingDirPublish;
+    private String onboardingDirCurateStg;
+    private String onboardingDirPublishStg;
+
+
+    @Inject
+    private static Logger logger;
+
+
+    String wbFilePath = "./src/test/resources/fixtures/ME_FIN_Cash_Detail.xlsx";
     File wbfile = new File(wbFilePath);
+
+    private static ResourceBundle rb = ResourceBundle.getBundle("cif");
 
     @Before
     public void setUp() throws Exception {
         // Place workbook .xlsx file at hdfs://dlap_tst/cif/onboarding/
 
+//        System.out.println("-->"+rb.getString("onboarding.dir"));
+        onboardingDir=rb.getString("onboarding.dir").trim();
+//        onboardingDirPublish=rb.getString("onboarding.dir.publish");
+        onboardingDirCurateStg=rb.getString("onboarding.dir.curate.stg")+"/*".trim();
+        onboardingDirPublishStg=rb.getString("onboarding.dir.publish.stg").trim();
+
+
+        File curateStg = new File(onboardingDirCurateStg);
+        File publishStg = new File(onboardingDirPublishStg);
+        File onboardingDirectory = new File(onboardingDir);
+//        File onboardingDirectoryPublish = new File(onboardingDirPublish);
+
+        FileUtils.copyFile(curateStg, onboardingDirectory);
+        FileUtils.copyDirectory(publishStg, onboardingDirectory);
+
         // Sleep thread ?  I don't think there's a notification to plug in...
         Thread.sleep(30000);
+
+        String[] extensions = new String[]{"xlsx"};
+        List<File> afterOnboardingFilelist = (List<File>) FileUtils.listFiles(new File(onboardingDir), extensions, true);
+
+        for (File file : afterOnboardingFilelist) {
+
+            logger.info("***************** FILE NAME:=====>" + file.getName() + "  *******************");
+            assertTrue(new File(file.getAbsolutePath() + ".completed").exists());
+        }
 
         //Check for .completed file creation
         String completedFileName = wbFilePath+".completed";
@@ -49,13 +95,14 @@ public class WorkbookStepDefs {
 
         // throw exception if file not found or .error file found instead
 
-//
-//        // Set-up REST client
-//        restClient = CIFInjector.createInstance(NavigatorRestClient.class);
-//        restClient.setUsername("csaload1");
-//        restClient.setPasswordPlain("C$@l0adP120d");
-//        restClient.setPasswordEncrypted("SthNm1MbsRMNcBUYw88hbA==:/crmsdMrCILWlZeaouNiMA==");
+
+        // Set-up REST client
+        restClient = CIFInjector.createInstance(NavigatorRestClient.class);
+        restClient.setUsername("csaload1");
+        restClient.setPasswordPlain("C$@l0adP120d");
+        restClient.setPasswordEncrypted("SthNm1MbsRMNcBUYw88hbA==:/crmsdMrCILWlZeaouNiMA==");
     }
+
 
     @Given("^I have parsed a workbook$")
     public void some_start_condition() throws Throwable {
