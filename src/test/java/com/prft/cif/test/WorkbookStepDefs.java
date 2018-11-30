e7ceb3e33479eb778687a6820ca97447b994c55b
Warner <cwarne01@bcbsmamd.net>
2018-11-30 18:01:03 -0500

    impala drop tables and scd

--- src/test/java/com/prft/cif/test/WorkbookStepDefs.java
+++ src/test/java/com/prft/cif/test/WorkbookStepDefs.java
@@ -348,7 +348,7 @@ public class WorkbookStepDefs {
             stmt = conn.createStatement();
             String dropSql;
             dropSql = "drop table "+finalTableName;
-            String dropScdSql = "drop table "+finalTableName+"_scd";
+            String dropScdSql = "drop table if exists"+finalTableName+"_scd";
             System.out.println(dropSql);
             System.out.println(dropScdSql);
             stmt.execute(dropSql);
