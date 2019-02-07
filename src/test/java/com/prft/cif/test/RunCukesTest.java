3ca50691d4fd0e11e68509b692f03d7d543dbab2
Darshan Patel <darshan.patel@bcbsma.com>
2019-02-07 13:05:36 -0500

    change in RunCucktest

--- src/test/java/com/prft/cif/test/RunCukesTest.java
+++ src/test/java/com/prft/cif/test/RunCukesTest.java
@@ -7,13 +7,13 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"json","json:target/cucumber-reports/cucumber.json"},
-//        tags = {"~@Ignore","@SmokeTest"},
+        tags = {"~@Ignore","@SmokeTest"},
         features = "src/test/resources/com/prft/cif/test/"
         )
 public class RunCukesTest {
         public static void main(String[] args) throws Throwable {
-//                String[] arguments = {"--tags @SmokeTest"};
-                String[] arguments = {};
+                String[] arguments = {"--tags @SmokeTest"};
+//                String[] arguments = {};
                 cucumber.api.cli.Main.main(arguments);
                         cucumber.api.cli.Main.main(arguments);
         }
