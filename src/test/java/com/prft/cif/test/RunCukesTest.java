70d3ff4a0caf53492a010f2c13163c39856ab4ec
Darshan Patel <darshan.patel@bcbsma.com>
2019-02-07 16:31:33 -0500

    Add data-ingestion4

--- src/test/java/com/prft/cif/test/RunCukesTest.java
+++ src/test/java/com/prft/cif/test/RunCukesTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"json","json:target/cucumber-reports/cucumber.json"},
         tags = {"~@Ignore","@SmokeTest","@DataTest"},
-        features = "src/test/resources/com/prft/cif/test/workbook.feature"
+        features = "src/test/resources/com/prft/cif/test/"
         )
 public class RunCukesTest {
 /*        public static void main(String[] args) throws Throwable {
